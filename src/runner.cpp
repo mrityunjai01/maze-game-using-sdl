bfd564ff5056a23563f0ce75e63cc3030c09ce89
Mrityunjai Singh <mrityunjaisingh3333@gmail.com>
2022-03-31 01:17:32 +0530

    speed change

--- src/runner.cpp
+++ src/runner.cpp
@@ -15,8 +15,8 @@ float Runner::get_speed() {
   return speed_multiplier * (float) speed;
 }
 void Runner::step() {
-  pos.x += (dir_x * float get_speed());
-  pos.y += (dir_y * float get_speed());
+  pos.x += (dir_x * get_speed());
+  pos.y += (dir_y * get_speed());
   // std::cout << "new pos " << pos.x << ", " << pos.y << '\n';
   current_x = (current_x + 75) % 600;
 
@@ -27,10 +27,10 @@ void Runner::setDir(float to_x, float to_y) {
   if (l < 1e-4) {
     return;
   }
-  std::cout <<"l i s" << l << '\n';
+  // std::cout <<"l i s" << l << '\n';
   dir_x = ( to_x - pos.x)/l;
   dir_y = ( to_y - pos.y)/l;
-  std::cout << "the direction is "<< dir_x <<", "<< dir_y<<'\n';
+  // std::cout << "the direction is "<< dir_x <<", "<< dir_y<<'\n';
 
 
 }
